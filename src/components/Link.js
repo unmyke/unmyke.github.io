6adf83013f46e22256b773b880b9d5746315d48b
unmyke <unmyke@gmail.com>
2020-01-13 16:54:50 +0800

    feat(components): add Link component

---
+++ src/components/Link.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link as GatsbyLink } from "gatsby"
+import styles from "./Link.module.scss"
+
+const Link = ({ children, to, ...other }) => {
+  const isInternal = /^\/(?!\/)/.test(to)
+
+  return isInternal ? (
+    <GatsbyLink className={styles["inner"]} to={to}>
+      {children}
+    </GatsbyLink>
+  ) : (
+    <a className={styles["outer"]} href={to} {...other}>
+      {children}
+    </a>
+  )
+}
+
+export default Link
