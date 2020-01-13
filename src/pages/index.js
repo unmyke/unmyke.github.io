20807f187f09a7fcfc4b97dc5908320971fe3dd3
unmyke <unmyke@gmail.com>
2020-01-13 17:00:16 +0800

    feat(pages): add index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react"
+import Template from "../components/Template"
+import Header from "../components/Header"
+import References from "../components/References"
+import { usePageQuery } from "../utils"
 
-export default () => <div>Hello world!</div>
+const PageIndex = () => {
+  const { pages = [], global, links } = usePageQuery()
+  const currentPageIndex = 0
+  const currentPage = pages[currentPageIndex]
+
+  return (
+    <Template global={global} currentPage={currentPage}>
+      <Header links={links} currentLinkIndex={currentPageIndex} />
+      <p>Vox, vox, praeterea nihil&#9786;.</p>
+      <References />
+    </Template>
+  )
+}
+export default PageIndex
