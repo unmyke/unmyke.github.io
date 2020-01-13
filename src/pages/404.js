b81f79ec2e84b737dc9b4ddefd70eb84eb4e5873
unmyke <unmyke@gmail.com>
2020-01-13 16:59:52 +0800

    feat(pages): add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Template from "../components/Template"
+import Link from "../components/Link"
+
+const Page404 = () => (
+  <Template>
+    <Link to="/"><p>Ex nihilo nihil fit...</p></Link>
+  </Template>
+)
+export default Page404
