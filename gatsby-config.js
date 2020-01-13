6bbce1d25654bb9bb01f86e9da149ac782319f23
unmyke <unmyke@gmail.com>
2020-01-13 16:49:46 +0800

    feat(gql): add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,38 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    pages: [
+      {
+        link: {
+          pathname: "/",
+          name: "Mikhail Unenov",
+        },
+        title: "unmyke / Home",
+        description: "unmyke's personal website",
+      },
+      {
+        link: {
+          pathname: "/cv",
+          name: "CV",
+        },
+        title: "unmyke / CV",
+        description: "unmyke's cv",
+      },
+    ],
+    global: {
+      author: "unmyke",
+      siteUrl: "https://unmyke.github.com",
+    },
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: "src/utils/typography",
+      },
+    },
+  ],
 }
