7ebc2f098146a7601a7e1a814a0c220dc3dff209
unmyke <unmyke@gmail.com>
2021-07-26 17:29:50 +0300

    refactor: remove useless utils

--- src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export { default as getCurrentPageIndex } from "./get-current-page-index"
-export { default as typography } from "./typography"
-export { default as usePageQuery } from "./use-page-query"
